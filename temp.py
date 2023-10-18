d6264f157875f29a8aa3414e72f5cf291e3bfd3e
Grimm_mmirG <Theisaachill@gmail.com>
2023-10-18 15:22:36 -0600

    Added conversion from Fahrenheit to Celsius

--- temp.py
+++ temp.py
@@ -2,9 +2,16 @@ def celsius_to_farenheit(celsius):
     """ Given a celcius Value return a conversion"""
     fahrenheit = celsius * 9/5 + 32
     return fahrenheit
+def f_to_c(f):
+    """Given Farenheit value return conversion"""
+    celsius = (f-32) * 5/9
+    return celsius
 
 celsius = 25
-
+    
 # fahrenheit = celsius * 9/5 + 32
 fahrenheit = celsius_to_farenheit(celsius)
 print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
+
+celsius = f_to_c(fahrenheit)
+print(f"{fahrenheit} Fahrenheit to Celsius is {celsius}")
