42146941c1955b9d57cccc72e41b4570cf50ba3d
Grimm-mmirG <147000788+Grimm-mmirG@users.noreply.github.com>
2023-10-18 14:30:51 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,3 +1,24 @@
+# celsius = 25
+## fahrenheit = celsius * 9/5 + 32
+# ### print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
+
 celsius = 25
-fahrenheit = celsius * 9/5 + 32
-print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
+f = celsius * 9/5 +32
+
+
+print(f"{celsius} Celcius to Farenheit is {f}")
+
+
+
+def celsius_to_farenheit(f)
+  x= f * 9/5 + 32
+  return x
+
+Celsius = 25
+conversion = celsius_to_farenheit(f)
+
+print(f"{celsius} Celsius to Fahrenheit is {conversion}")
+
+
+
+
